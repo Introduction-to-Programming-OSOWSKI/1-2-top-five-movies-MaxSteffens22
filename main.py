533a2f235e3a0d29feb62bbfe394e6adfffcf674
MaxStefens22 <816467@mystma.org>
2021-09-23 14:48:42 -0500

    3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies():
     return ("retun of the jedi","the shawshank redemption","civilwar","the two towers","love simon")
-    print(topFiveMovies)
+    print (topFiveMovies)
     
